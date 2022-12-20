22cc0e0d4c24d8592ae52b6009038e5c5c95641f
Mel <74751155+anotruong@users.noreply.github.com>
2022-12-20 11:23:17 -0800

    working on validation

--- src/pages/components/forms.js
+++ src/pages/components/forms.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Input from './FormElements/input';
 import Button from './FormElements/button';
+import { VALIDATION_REQUIRED } from './validator/validators';
 
 import './stylesheets/forms.css';
 
@@ -12,23 +13,31 @@ const Forms = () => {
         type="text" 
         label="cardholder name" 
         placeholder={'e.g. Jane Appleseed'} 
+        validator={VALIDATION_REQUIRED}
+        errorText="Please enter a valid cardholder name"
       />
       <Input 
         element="input" 
         type="number" 
         label="card number" 
         placeholder={'e.g. 1234-5678-9123-0000'}
+        validator={VALIDATION_REQUIRED}
+        errorText={'Please enter a valid credit card.'}
       />
       <Input 
         element="input" 
         type="number" 
         label="exp.date (mm/yy)" 
+        validator={VALIDATION_REQUIRED}
+        errorText={'Please fill in the field'}
       />
       <Input 
         element="input" 
         type="number" 
         label="cvc" 
         placeholder={'e.g 123'}
+        validator={VALIDATION_REQUIRED}
+        errorText={'Please enter in a valid cvc.'}
       />
       <Button type="submit">confirm</Button> 
     </div>    
